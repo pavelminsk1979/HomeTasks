7bf531bcffb5ebebde1dc865b087fa6433b75618
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-02-12 13:20:09 +0300

    HW3 refactor2

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -7,7 +7,7 @@ type GreetingPropsType = {
     addUser: ()=>void // need to fix any
     onBlur: ()=>void // need to fix any
     onEnter: (e: KeyboardEvent<HTMLInputElement>)=>void // need to fix any
-    error: null|string // need to fix any
+    error: string // need to fix any
     totalUsers: number // need to fix any
     lastUserName?: string // need to fix any
 }
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    const inputClass =error? s.errorInput:'' // need to fix with (?:)
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -39,16 +39,16 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
 }) => {
     // деструктуризация пропсов
     const [name, setName] = useState<string>('') // need to fix any
-    const [error, setError] = useState<null|string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e:ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
 
-        error && setError(null)
+        error && setError('')
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
-        setTotalUsers(totalUsers+1)
+       // setTotalUsers(totalUsers+1)
     }
 
     const onBlur = () => {
@@ -59,8 +59,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnEnter(e, addUser)
     }
 
-    const [totalUsers,setTotalUsers] = useState(0)  // need to fix
-    const lastUserName = users[0]&&users[0].name // need to fix
+    const totalUsers = users.length // need to fix
+    const lastUserName = users[0] && users[0].name // need to fix
 
     return (
         <Greeting
